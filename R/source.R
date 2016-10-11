f619aabab051fe89111d35499cd85b6f796d8a18
Wanja Moessing <wanjas_github@outlook.com>
2016-10-11 23:48:13 +0200

    Omit TTL output if no TTL was sent.
    * Thereby output will be exactly as in Hedderik's version if no triggers where sent to the Host PCs parallel port.

--- R/source.R
+++ R/source.R
@@ -8,6 +8,12 @@ readEDF <- function(name, checkConsistency=FALSE) {
       retVal$events$msg <- as.character(retVal$events$msg)
       ## Remove empty events:
       retVal$events <- retVal$events[retVal$events$msg != "",]
+      ## Delete TTL triggers if none present
+      if(!any(retVal$input$ttl!=0)){
+        retVal$input <- NULL
+        retVal$samples$InputS <- NULL
+        cat("No TTL triggers found. Now assuming no input was sent to the parallel port. Deleting the corresponding column in the output.")
+      }
     } else {
         warning(paste0("Specified file (",name,") not found."))
     }
